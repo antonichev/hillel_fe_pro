fc401135d3366386d7598422f839e3ea2171f0bf
Antonichev Maksym <novomax@ukr.net>
2022-04-29 19:42:12 +0300

    Add L8_290422

---
+++ L8_290422/Task2/index.js
@@ -0,0 +1,16 @@
+
+function exponentiate(arr, pow) {
+
+  var newArr = [];
+
+  for(item of arr) {
+    newArr.push(item ** pow);
+  }
+
+  return newArr;
+
+}
+
+var res = exponentiate([2, 4, 3, 1], 2);
+
+console.log(res);
